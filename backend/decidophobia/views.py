91af17d37040f8832ec74e011f6aa8f7797b7efe
Faisal <runescape1724@gmail.com>
2024-02-04 17:00:06 -0500

    Signup working and redirects to home page

--- backend/decidophobia/views.py
+++ backend/decidophobia/views.py
@@ -1,4 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+# from . forms import CreateUserForm, CreateLoginForm
+from django.contrib.auth.models import auth
+from django.contrib.auth import authenticate
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login as auth_login
+
 
 def hello_world(request):
     return render(request, 'temp.html')
@@ -7,4 +15,37 @@ def home(request):
     return render(request, 'home.html')
 
 def login(request):
+    # form = CreateLoginForm()
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            auth_login(request, user)
+            messages.success(request, 'Login successful.')
+            return redirect('home') 
+        else:
+            messages.error(request, 'Invalid username or password.')
+
     return render(request, 'login.html')
+
+def signup(request):
+    if request.method == 'POST':
+        print("Form submitted!")
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            print("Form submitted!")
+            user = form.save()
+            username = form.cleaned_data.get('username')
+            auth_login(request, user)
+            messages.success(request, f'Signup successful. Welcome, {username}!')
+            return redirect('home')  
+        else:
+            print(form.errors)
+            messages.error(request, 'Signup failed. Please correct the errors in the form.')
+
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'signup.html', {'form': form})
