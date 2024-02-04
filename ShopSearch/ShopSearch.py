a5a7dac395c79e31bc6fe8d116da119fe2b79e17
ahmed33033 <ahmed.akod@outlook.com>
2024-02-04 18:38:21 -0500

    [SCRUM-45-external-api] API file ShopSearch.py can now mint access tokens succesfully based on whether the last minted token was expired

--- ShopSearch/ShopSearch.py
+++ ShopSearch/ShopSearch.py
@@ -2,24 +2,54 @@ import requests
 import json
 from datetime import datetime, UTC, timedelta
 
-token_mint_body = {"ebay": {"grant_type": "client_credentials", "scope": "https://api.ebay.com/oauth/api_scope"}}
-def shop_search(item_name, num_items):
+import pytz
 
-def get_token():
-    with open("api_config.json", 'r') as config_file:
-        config = json.load(config_file)
+date_format = "%Y/%m/%d %H:%M:%S %z"
 
-    expiry_time = datetime.strptime(config.get("last_gen"), "%Y/%m/%d %H:%M:%S")
-    current_time = datetime.now(UTC)
-    if expiry_time < current_time:
-        token = mint_token(config)
-        config["token_expiry"] = datetime.now() +
-        print("New access token minted")
+def mint_ebay_token(ebay_config):
+    # make api call to get token, along with important details regarding it
+    token_response = requests.post(ebay_config.get("url"),
+                                   data=ebay_config.get("body"),
+                                   headers=ebay_config.get("headers"))
+
+    if token_response.status_code == 200:
+        token_details = json.loads(token_response.content)
+        # update the config file to reflect the request for a new token
+        ebay_config["token_expiry"] = (datetime.now(UTC) +
+                                       timedelta(seconds=int(token_details.get("expires_in")))).strftime(date_format)
+        ebay_config["token"] = token_details.get("access_token")
+        return True
     else:
-        token = config.get("token")
-def mint_token(config):
-    token_response = requests.post(config.get("url"), data=token_mint_body.get("ebay"),
-                               headers={"Content-Type": config.get("Content-Type"),
-                                        "Authorization": config.get("token_auth")})
-    token_details = json.loads(token_response.content)
-    config["token_expiry"] = datetime.now() + token_details.get("expires_in")
+        return False
+
+
+def get_ebay_token(force_new_token=False):
+    with open("api_config.json", 'r') as config_file:
+        all_config = json.load(config_file)
+        ebay_config = all_config.get("ebay")
+
+    expiry_time = datetime.strptime(ebay_config.get("token_expiry"), date_format)
+    current_time = datetime.now(UTC)
+    if expiry_time < current_time or force_new_token:
+        if mint_ebay_token(ebay_config):
+            print("New access token minted")
+
+            # rewrite config file with new token
+            all_config["ebay"] = ebay_config
+            with open("api_config.json", "w") as config_file:
+                config_file.write(json.dumps(all_config))
+        else:
+            print("Couldn't acquire access token. Sorry!")
+            return -1
+
+    return ebay_config.get("token")
+
+
+def shop_search(shop_name = "ebay", item_name="Naruto", num_items="10", force_new_token = False):
+    token = get_ebay_token(force_new_token)
+    if token == -1: return []
+
+
+
+if __name__ == "__main__":
+    shop_search(force_new_token=True)
