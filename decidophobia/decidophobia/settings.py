2f0800e202871ebdca17edfb3e032be3aff69fb7
Wu Hung Mao <hongmao.wu@mail.utoronto.ca>
2024-02-04 15:47:05 -0500

    Almost finish the whole feature, need to send results to result page

--- decidophobia/decidophobia/settings.py
+++ decidophobia/decidophobia/settings.py
@@ -79,6 +79,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    },
+    'test_database': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / "scrum_13_questionnaire" / "test_database.db",
     }
 }
 
