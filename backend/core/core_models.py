b318e9decef3cbadc0052eb4820bceb93ddde8ef
Faisal <runescape1724@gmail.com>
2024-02-04 20:56:41 -0500

    defined shoppingCart and cartProduct models

--- backend/core/core_models.py
+++ backend/core/core_models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 class BaseModel(models.Model):
@@ -7,3 +8,19 @@ class BaseModel(models.Model):
 
     class Meta:
         abstract = True
+
+
+class CartProduct(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image_link = models.URLField()
+
+    def __str__(self):
+        return self.name
+
+class ShoppingCart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    products = models.ManyToManyField(CartProduct)
+
+    def __str__(self):
+        return f"Shopping Cart for {self.user.username}"
