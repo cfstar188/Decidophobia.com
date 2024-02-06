55280a49a4d872d37af5b90cecced3e96439c8dc
MintV-Vincent <117199249+MintV-Vincent@users.noreply.github.com>
2024-02-05 19:53:36 -0500

    Fix the merge between nextjs and django

--- backend/decidophobia/views.py
+++ backend/decidophobia/views.py
@@ -8,5 +8,50 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
 
+
+
 def hello_world(request):
     return render(request, 'temp.html')
+
+def home(request):
+    return render(request, 'home.html')
+
+def login(request):
+    # form = CreateLoginForm()
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            auth_login(request, user)
+            messages.success(request, 'Login successful.')
+            return redirect('home') 
+        else:
+            messages.error(request, 'Invalid username or password.')
+
+    return render(request, 'login.html')
+
+def logout(request):
+    auth_logout(request)
+    return redirect('home')
+
+def signup(request):
+    if request.method == 'POST':
+        print("Form submitted!")
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            print("Form submitted!")
+            user = form.save()
+            username = form.cleaned_data.get('username')
+            auth_login(request, user)
+            messages.success(request, f'Signup successful. Welcome, {username}!')
+            return redirect('home')  
+        else:
+            print(form.errors)
+            messages.error(request, 'Signup failed. Please correct the errors in the form.')
+
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'signup.html', {'form': form})
