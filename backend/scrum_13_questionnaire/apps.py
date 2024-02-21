c619e3640cf5be9f5780da55a14b96b949051d40
gaurav3247 <90786427+gaurav3247@users.noreply.github.com>
2024-02-21 12:55:11 -0500

    Delete backend directory

--- backend/scrum_13_questionnaire/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-class Scrum13QuestionnaireConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'scrum_13_questionnaire'
--- backend/shopping_list/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from shopping_list.views import AddShoppingItemView, ChangeQuantityView, DeleteShoppingItem, ShoppingListView
-
-urlpatterns = [
-    path('details/', ShoppingListView.as_view()),
-    path('add-product/', AddShoppingItemView.as_view()),
-    path('remove-product/', DeleteShoppingItem.as_view()),
-    path('change-quantity/', ChangeQuantityView.as_view()),
-]
