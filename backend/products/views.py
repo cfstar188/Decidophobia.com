dbf8540d27eb4e1daf6072c8d0e5c4e4b2c9b682
gaurav3247 <90786427+gaurav3247@users.noreply.github.com>
2024-02-23 15:21:22 -0500

    Update views.py

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
 from rest_framework.generics import ListAPIView, CreateAPIView, DestroyAPIView, UpdateAPIView
+from products.permissions import ProductPermissions
 from products.serializers import ProductSerializer
 
 # Create your views here.
 class CreateProductView(CreateAPIView):
     serializer_class = ProductSerializer
+    permission_classes = [ProductPermissions]
+
     
