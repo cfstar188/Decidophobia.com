3ca98b19dad24fd98dccf28e3847ef94062fe907
gaurav3247 <90786427+gaurav3247@users.noreply.github.com>
2024-02-23 15:22:05 -0500

    Created custom permissions for Products

---
+++ backend/products/permissions.py
@@ -0,0 +1,10 @@
+from rest_framework import permissions
+
+class ProductPermissions(permissions.BasePermission):
+    AdminKey = 'decidophobiaAdmin'
+
+    def has_permission(self, request, view):
+        if request.method == 'GET':
+            return False
+        else:
+            return request.headers.get('Key', '') == self.AdminKey
