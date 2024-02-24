14d0e03995e72c955964971d0bc8576d9ec5991d
Wu Hung Mao <hongmao.wu@mail.utoronto.ca>
2024-02-23 20:26:35 -0500

    Task 58 trying to start backend side

--- backend/decidophobia/settings.py
+++ backend/decidophobia/settings.py
@@ -39,9 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'decidophobia',
-    'shoppingCart',
-    "django_nextjs"
-    'shopping_list',
+    # 'shoppingCart',
+    "django_nextjs",
+    # 'shopping_list',
 ]
 
 MIDDLEWARE = [
