4426fc3d394a490122e0dbf87be932ca110ede03
Justin Tran <just.tran@mail.utoronto.ca>
2024-02-25 16:45:49 -0500

    Discussion Board scrum 66

--- backend/discussionBoard/urls.py
+++ backend/discussionBoard/urls.py
@@ -4,5 +4,6 @@ from . import views
 # URLConfigs
 urlpatterns = [
     path('messages/', views.messageBoard),
-    path('testReq/', views.requestTest)
+    path('testReq/', views.requestTest),
+    path('testo/', views.htmlAndCssTest),
 ]
