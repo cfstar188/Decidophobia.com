189493f135e83ff6bc60abb7b6998bb250a6c5d1
gaurav3247 <90786427+gaurav3247@users.noreply.github.com>
2024-03-10 21:28:48 -0400

    Delete frontend directory

--- frontend/Library/CreateFunction.tsx
+++
@@ -1,9 +0,0 @@
-export function createProduct(
-  product: string = "",
-  price: number,
-  currency: string = "",
-  score: number,
-  image: string = ""
-): any {
-  return { product, price, currency, score, image };
-}
