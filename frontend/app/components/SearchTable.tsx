7c874f62094e8abdec6eb9b1cbfaccbd64c98179
Gaurav Poona <gauravpoona3247@gmail.com>
2024-03-24 13:34:59 -0400

    Added alerts for search table

--- frontend/app/components/SearchTable.tsx
+++ frontend/app/components/SearchTable.tsx
@@ -5,6 +5,8 @@ import HorizontalSelectBar from "@/app/components/HorizontalSelectBar";
 import SquareCheckbox from "@/app/components/Button/SquareButton";
 import { allProductAtom } from "@/Library/SelectedAtom";
 import api from "../core/baseAPI";
+import Alerts from "./alerts";
+import { authAtom } from "@/Library/AuthAtom";
 
 interface Product {
   name: string;
@@ -15,33 +17,49 @@ interface Product {
 }
 
 export function SearchTable() {
+  const [auth] = useAtom(authAtom);
   const [products] = useAtom(allProductAtom);
   const [tester, setTester] = useState();
   // I have set the buy button to this state, but I think you might need a state that updates inside a useeffect that calls your api.
   // This is just I thought since I have been doing a lot of react learning for this project. So you might know a better way.
   const [buy, setBuyState] = useState(0);
+  const [notLoggedInAlertOpen, setNotLoggedInAlert] = useState(false);
+  const [showAddedToCartAlert, setShowAddedToCartAlert] = useState(false);
+  const [itemAlreadyInCartAlert, setitemAlreadyInCartAlert] = useState(false);
 
   useEffect(() => {}, [products]);
 
   function handleBuy(product: Product) {
-    api.post("/products/create-product/", {
-      name: product.name,
-      company: product.company,
-      price: product.price,
-      preview_picture: product.picture,
-    }, {
-      headers: {
-        'Key': 'decidophobiaAdmin'
-      }
-    })
-    .then((response) => {
-      console.log(response.data.id);
-      api.post("/shopping-list/add-item/", {
-        product_id: response.data.id,
-        quantity: 1,
+    console.log('auth', auth)
+    if (!auth.isAuthenticated) {
+      setNotLoggedInAlert(true);
+    }
+    else {
+      api.post("/products/create-product/", {
+        name: product.name,
+        company: product.company,
+        price: product.price,
+        preview_picture: product.picture,
+      }, {
+        headers: {
+          'Key': 'decidophobiaAdmin'
+        }
+      })
+      .then((response) => {
+        console.log(response.data.id);
+        api.post("/shopping-list/add-item/", {
+          product_id: response.data.id,
+          quantity: 1,
+        })
+          .then((response) => {
+            setBuyState(1);
+            setShowAddedToCartAlert(true);
+          })
+          .catch((error) => {
+            setitemAlreadyInCartAlert(true)
+          });
       });
-      setBuyState(1);
-    });
+    }
   }
 
   return (
@@ -83,6 +101,15 @@ export function SearchTable() {
           </div>
         ))}
       </div>
+      <Alerts message="Login to add to cart!" severity="error"
+              isOpen={notLoggedInAlertOpen}
+              onClose={() => setNotLoggedInAlert(false)} />
+      <Alerts message="Added to cart!" severity="success"
+              isOpen={showAddedToCartAlert}
+              onClose={() => setShowAddedToCartAlert(false)} />
+      <Alerts message="Product already in cart!" severity="error"
+              isOpen={itemAlreadyInCartAlert}
+              onClose={() => setitemAlreadyInCartAlert(false)} />
     </>
   );
 }
